2973d7c56f51391e6f4d199268894fa308d32425
Alexksandr <archangel7017@gmail.com>
2017-07-08 21:50:14 +0300

    Initial commit

---
+++ src/Country.java
@@ -0,0 +1,48 @@
+/**
+ * Created by Александр on 08.07.2017.
+ */
+public class Country {
+
+    String name;
+    long population;
+
+    public Country(String name, long population) {
+        super();
+        this.name = name;
+        this.population = population;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public long getPopulation() {
+        return population;
+    }
+    public void setPopulation(long population) {
+        this.population = population;
+    }
+
+    // если длина имени в объекте Country - четное число,
+    // то возвращаем 31(любое случайное число), а если нечетное - 95 (любое случайное число).
+    // указанный ниже метод - это не самый лучший способ генерации хеш-кода,
+    // но мы воспользуемся им для более четкого понимания хеш-карт.
+
+    @Override
+    public int hashCode() {
+        if(this.name.length()%2==0)
+            return 31;
+        else
+            return 95;
+    }
+    @Override
+    public boolean equals(Object obj) {
+
+        Country other = (Country) obj;
+        if (name.equalsIgnoreCase((other.name)))
+            return true;
+        return false;
+    }
+}
+
